83c8f04ea745a53f60630836c2b77baa8414844a
Aaron O'Mullan <aaron.omullan@gmail.com>
2016-10-24 10:31:33 +0200

    Add parallel benchmarks to test for lock contention

--- mutexes_test.go
+++ mutexes_test.go
@@ -3,7 +3,7 @@ package syncgroup
 import (
 	"strconv"
 	"sync"
-	"time"
+	//	"time"
 
 	"testing"
 )
@@ -40,48 +40,54 @@ func BenchmarkReadGroupLockUnlock(b *testing.B) {
 	}
 }
 
-func TestRwMutexes(t *testing.T) {
-	N := 100
-	M := 500
+// BenchmarkGroupMutexes
+// Runs 100 go-routines read locking/unlocing on a single key (at a time)of a group mutex
+func BenchmarkGroupMutexes(b *testing.B) {
+	M := 100
 
 	locks := NewMutexGroup()
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < N; i++ {
+	for i := 0; i < b.N; i++ {
 		key := strconv.Itoa(i)
 		for j := 0; j < M; j++ {
 			wg.Add(1)
 			// Lock, sleep
 			go func(key string) {
 				locks.RLock(key)
-				time.Sleep(1 * time.Millisecond)
+				//time.Sleep(1 * time.Millisecond)
 				locks.RUnlock(key)
 				wg.Done()
 			}(key)
 		}
-	}
 
-	// Wait for all to finish
-	wg.Wait()
+		// Wait for all to finish
+		wg.Wait()
+	}
 }
 
-func TestEmptyParallel(t *testing.T) {
-	N := 100
-	M := 500
+// BenchmarkEmptyParallel
+// Runs 100 go-routines read locking/unlocing on a single mutex
+func BenchmarkEmptyParallel(b *testing.B) {
+	M := 100
 
+	lock := sync.RWMutex{}
 	wg := sync.WaitGroup{}
 
-	for i := 0; i < N; i++ {
+	for i := 0; i < b.N; i++ {
+		key := strconv.Itoa(i)
 		for j := 0; j < M; j++ {
 			wg.Add(1)
 			// Lock, sleep
-			go func() {
-				time.Sleep(1 * time.Millisecond)
+			go func(key string) {
+				lock.RLock()
+				//time.Sleep(1 * time.Millisecond)
+				lock.RUnlock()
 				wg.Done()
-			}()
+			}(key)
 		}
-	}
 
-	// Wait for all to finish
-	wg.Wait()
+		// Wait for all to finish
+		wg.Wait()
+	}
 }
