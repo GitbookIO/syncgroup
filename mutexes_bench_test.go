22a877d3872ef106539b17c3f6971d024135aa6a
Aaron O'Mullan <aaron.omullan@gmail.com>
2017-01-04 15:11:36 +0100

    mutexes: add BenchmarkParallelSharded

--- mutexes_bench_test.go
+++ mutexes_bench_test.go
@@ -66,6 +66,32 @@ func BenchmarkParallelGroup(b *testing.B) {
 	}
 }
 
+// BenchmarkParallelSharded
+// Runs 100 go-routines read locking/unlocing on a single key (at a time) of a Sharded mutex
+func BenchmarkParallelSharded(b *testing.B) {
+	M := 100
+
+	locks := NewShardedMutexGroup()
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < b.N; i++ {
+		key := strconv.Itoa(i)
+		for j := 0; j < M; j++ {
+			wg.Add(1)
+			// Lock, sleep
+			go func(key string) {
+				locks.RLock(key)
+				//time.Sleep(1 * time.Millisecond)
+				locks.RUnlock(key)
+				wg.Done()
+			}(key)
+		}
+
+		// Wait for all to finish
+		wg.Wait()
+	}
+}
+
 // BenchmarkParallelSingle
 // Runs 100 go-routines read locking/unlocing on a single mutex
 func BenchmarkParallelSingle(b *testing.B) {
