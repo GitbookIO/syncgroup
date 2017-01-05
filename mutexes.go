01435f2eb23bd200b52a8e1fdfc69ce1afe1b010
Aaron O'Mullan <aaron.omullan@gmail.com>
2017-01-05 14:57:51 +0100

    mutexes.go: use more optimized rwmutex

--- mutexes.go
+++ mutexes.go
@@ -9,57 +9,68 @@ import (
 type MutexGroup struct {
 	lock    *sync.RWMutex
 	active  *ActiveGroup
-	mutexes map[string]*sync.RWMutex
+	mutexes map[string]*rwmutex
 }
 
 func NewMutexGroup() *MutexGroup {
 	return &MutexGroup{
 		lock:    &sync.RWMutex{},
 		active:  NewActiveGroup(),
-		mutexes: map[string]*sync.RWMutex{},
+		mutexes: map[string]*rwmutex{},
 	}
 }
 
 func (mg *MutexGroup) Lock(key string) {
-	mg.inc(key)
 	mg.getOrCreate(key).Lock()
 }
 
 func (mg *MutexGroup) RLock(key string) {
-	mg.inc(key)
 	mg.getOrCreate(key).RLock()
 }
 
 func (mg *MutexGroup) Unlock(key string) {
-	mg.getOrFail(key).Unlock()
-	mg.dec(key)
+	mutex := mg.getOrFail(key)
+	usage := mutex.Unlock()
+	if usage == 0 {
+		mg.maybeDelete(key, mutex)
+	}
 }
 
 func (mg *MutexGroup) RUnlock(key string) {
-	mg.getOrFail(key).RUnlock()
-	mg.dec(key)
+	mutex := mg.getOrFail(key)
+	usage := mutex.RUnlock()
+	if usage == 0 {
+		mg.maybeDelete(key, mutex)
+	}
 }
 
 func (mg *MutexGroup) Has(key string) bool {
 	return mg.active.Has(key)
 }
 
-func (mg *MutexGroup) inc(key string) {
-	mg.lock.RLock()
-	defer mg.lock.RUnlock()
-	mg.active.Inc(key)
-}
-
-func (mg *MutexGroup) dec(key string) {
-	mg.lock.Lock()
-	defer mg.lock.Unlock()
-	// No longer active
-	if mg.active.Dec(key) == 0 {
-		delete(mg.mutexes, key)
+func (mg *MutexGroup) maybeDelete(key string, mutex *rwmutex) bool {
+	// Is in use
+	if mutex.count.Get() != 0 {
+		return false
 	}
+	// Track if deleted
+	deleted := false
+	// Lock
+	mutex.Lock()
+	// Delete if only used by us
+	if mutex.count.Get() == 1 {
+		deleted = true
+		// Delete from map
+		mg.lock.Lock()
+		delete(mg.mutexes, key)
+		mg.lock.Unlock()
+	}
+	mutex.Unlock()
+
+	return deleted
 }
 
-func (mg *MutexGroup) getOrFail(key string) *sync.RWMutex {
+func (mg *MutexGroup) getOrFail(key string) *rwmutex {
 	mg.lock.RLock()
 	defer mg.lock.RUnlock()
 	// Get
@@ -69,13 +80,14 @@ func (mg *MutexGroup) getOrFail(key string) *sync.RWMutex {
 	panic(fmt.Sprintf(`MutexGroup.getOrFail("%s"): Tried to perform an Unlock on a key that was never Locked`, key))
 }
 
-func (mg *MutexGroup) getOrCreate(key string) *sync.RWMutex {
+func (mg *MutexGroup) getOrCreate(key string) *rwmutex {
+
 	mg.lock.Lock()
 	defer mg.lock.Unlock()
 
 	// Create if doesn't exist
 	if _, ok := mg.mutexes[key]; !ok {
-		mg.mutexes[key] = &sync.RWMutex{}
+		mg.mutexes[key] = &rwmutex{}
 	}
 
 	return mg.mutexes[key]
